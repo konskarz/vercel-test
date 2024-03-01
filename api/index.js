ade270694013e3535191cc0896b2766a30e7845b
Konstantin Karzanov <k.karzanov@gmail.com>
2024-03-01 17:17:26 +0100

    move app to src

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const app = Fastify({
   logger: true,
 })
 
-app.register(import("./app.js"))
+app.register(import("../src/app.js"))
 
 export default async function handler(req, reply) {
   await app.ready()
