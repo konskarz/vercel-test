2a4fafff2da46bfef68dab7a4a461260d27810cb
Konstantin Karzanov <k.karzanov@gmail.com>
2024-03-02 22:01:24 +0100

    add task routes & swagger

--- src/app.js
+++ src/app.js
@@ -14,5 +14,30 @@ export default async function app (fastify, options) {
   fastify.register(import('@fastify/jwt'), {
     secret: process.env.JWT_SECRET
   })
+  fastify.register(import('@fastify/swagger'), {
+    openapi: {
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT'
+          }
+        }
+      }
+    }
+  })
+  // 1.16.0 works - https://github.com/scalar/scalar/issues/1042
+  fastify.register(import('@scalar/fastify-api-reference'), {
+    routePrefix: '/docs',
+    configuration: {
+      theme: 'default' // https://github.com/scalar/scalar/issues/839
+    }
+  })
+
   fastify.register(import('./routes/user.js'))
+  fastify.register(async function authenticated (fastify, options) {
+    fastify.addHook('onRequest', (request) => request.jwtVerify())
+    fastify.register(import('./routes/task.js'), { prefix: '/tasks' })
+  })
 }
