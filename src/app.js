1a59b999e05cedead942b13add1ddc68e6463abc
Konstantin Karzanov <k.karzanov@gmail.com>
2024-07-18 11:34:50 +0200

    fix root

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ import mapper from '@platformatic/sql-mapper'
 
 /** @param {import('fastify').FastifyInstance} fastify */
 export default async function app (fastify, options) {
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
   fastify.register(mapper.plugin, {
     connectionString: process.env.DATABASE_URL
   })
@@ -28,7 +25,7 @@ export default async function app (fastify, options) {
     }
   })
   fastify.register(import('@fastify/swagger-ui'), {
-    routePrefix: '/docs'
+    routePrefix: '/'
   })
 
   fastify.register(import('./routes/user.js'))
