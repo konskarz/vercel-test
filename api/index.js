768688e2c990233efa67aa28fb2189b4b6555d49
Konstantin Karzanov <k.karzanov@gmail.com>
2024-07-18 15:39:48 +0200

    fix api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import Fastify from 'fastify'
 
 const app = Fastify({ logger: true })
-app.register(import('../app'))
+app.register(import('../app/index.js'))
 
 export default async function handler(req, reply) {
   await app.ready()
